e76444ccd1e7a5b59834f12d0389e21610bb24d8
codth <codth@DESKTOP-8SJIFSI>
2024-02-14 20:48:57 -0800

    Initialization

--- src/application/main.java
+++ src/application/main.java
@@ -5,8 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		Excel obj = new Excel();
 		
-		obj.WriteExcel("Sheet1", 1, 1, "test");
-		System.out.print(obj.ReadExcel("Sheet1", 1, 1));
-		//ffhf
+		obj.WriteExcel("Victims", 2, 1, "Hey");
+		System.out.print(obj.ReadExcel("Victims", 1, 3));
+		//ffhff
 	}
 }
