a8cd5e29994990cb278355b3462333dba5d20248
alex <alex@DESKTOP-7J0JK5U>
2024-04-04 16:44:11 -0700

    Added support page

---
+++ src/application/SupportController.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.time.LocalDate;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class SupportController {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	@FXML
+    public void switchToHomePage(ActionEvent event) throws IOException {
+            root = FXMLLoader.load(getClass().getResource("HomePage.FXML"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+    }
+	
+}
