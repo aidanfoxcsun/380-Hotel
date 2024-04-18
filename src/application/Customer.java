d97fc5c951bdb6cc4a3620fcdc3210403ed02f98
maybe-aidan <afmaelstrom@gmail.com>
2024-04-17 17:49:17 -0700

    Updated Customer class to interface with Excel spreadsheet

--- src/application/Customer.java
+++ src/application/Customer.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.time.LocalDate;
+
 public class Customer {
     private String CustomerFirstName;
     private String CustomerLastName;
     private String CustomerEmail;
     private String CustomerPhoneNumber;
-    private String CustomerHotelRoom;
+    private HotelRoom CustomerHotelRoom;
     private String CustomerCreditCard;
+    
+    public int row;
     
     
     public Customer() {
@@ -42,12 +46,15 @@ public class Customer {
 		CustomerFirstName = customerFirstName;
 	}
 
-	public String getCustomerHotelRoom() {
+	public HotelRoom getCustomerHotelRoom() {
 		return CustomerHotelRoom;
 	}
 
 	public void setCustomerHotelRoom(String customerHotelRoom) {
-		CustomerHotelRoom = customerHotelRoom;
+		CustomerHotelRoom = HotelRoom.FindRoom(customerHotelRoom);
+		if(CustomerHotelRoom == null) {
+			System.err.println("ROOM NOT FOUND: " + customerHotelRoom);
+		}
 	}
 
 	public String getCustomerPhoneNumber() {
@@ -72,6 +79,74 @@ public class Customer {
 
 	public void setCustomerCreditCard(String customerCreditCard) {
 		CustomerCreditCard = customerCreditCard;
+	}
+	
+	public void UpdateExcel() {
+		Excel e = new Excel();
+		int i = 1;
+		while(e.getCell("Victims", i, 0) != null) {
+			if(e.getCell("Victims", i, 0).getStringCellValue().equals(CustomerFirstName)) {
+				this.row = i; // stores the location
+				return;
+			}
+			i++; 
+		}
+		this.row = i;
+		e.CreateCell("Victims", this.row, 0, CustomerFirstName);
+		e.CreateCell("Victims", this.row, 1, CustomerLastName); 
+		e.CreateCell("Victims", this.row, 2, CustomerEmail);
+		e.CreateCell("Victims", this.row, 3, CustomerPhoneNumber);
+		e.CreateCell("Victims", this.row, 6, CustomerCreditCard);
+		this.UpdateRoom(this.CustomerHotelRoom, null, null);
+	}
+	
+	public void UpdateRoom(HotelRoom room, String checkIn, String checkOut) {
+		Excel e = new Excel();
+		e.CreateCell("Victims", this.row, 10, room.getRoomID() + "");
+		e.CreateCell("Victims", this.row, 11, room.getHotelType().toString());
+		e.CreateCell("Victims", this.row, 12, room.getHotelCost() + "");
+		if(checkIn != null)
+			e.CreateCell("Victims", this.row, 13, checkIn.toString());
+		if(checkOut != null)
+			e.CreateCell("Victims", this.row, 14, checkOut.toString());
+	}
+	
+	public void Remove() {
+		Excel e = new Excel();
+		for(int i = 0; i < 20; i++) {
+			if(e.getCell("Victims", row, i) != null && e.getCell("Victims", row, i).getStringCellValue() != "") {
+				e.DeleteCell("victims", row, i);
+			}
+		}
+	}
+	
+	public static Customer findCustomer(String lastName, int roomID) {
+		Excel e = new Excel();
+		int i = 1;
+		while(e.getCell("Victims", i, 0) != null && e.getCell("Victims", i, 0).getStringCellValue() != "") {
+			System.out.println("Checking row: " + i + "; " + e.getCell("Victims", i, 1).getStringCellValue());
+			if((e.getCell("Victims", i, 1).getStringCellValue().equals(lastName)) &&
+					Integer.parseInt(e.getCell("Victims", i, 10).getStringCellValue()) == roomID) {
+				System.out.println("Customer found!");
+				return new Customer(e.getCell("Victims", i, 0).getStringCellValue(),
+						e.getCell("Victims", i, 1).getStringCellValue(),
+						e.getCell("Victims", i, 2).getStringCellValue(),
+						e.getCell("Victims", i, 3).getStringCellValue(),
+						e.getCell("Victims", i, 10).getStringCellValue(),
+						e.getCell("Victims", i, 6).getStringCellValue());
+			}
+			i++;
+		}
+		
+		System.out.println("No customer found!");
+		return null;
+	}
+	
+	@Override
+	public String toString() {
+		String card = "****";
+		card += CustomerCreditCard.substring(CustomerCreditCard.length() - 4, CustomerCreditCard.length());
+		return "[ " + CustomerFirstName + " " + CustomerLastName + ", EMAIL: " + CustomerEmail + ", PHONE: " + CustomerPhoneNumber + ", CARD: " + card  + ", ROOM: " + CustomerHotelRoom + " ]";
 	}
     
 }
--- src/application/main.java
+++ src/application/main.java
@@ -10,7 +10,7 @@ public class main{
 	public static void main(String[] args) {
 		// Test code
 		// Uncomment to test excel functionality
-		
+		/*
 		HotelRoom r1 = new HotelRoom(RoomTypes.SINGLE, 123, 100); // creates three new rooms with different types, costs, and types.
 		r1.AddRoom();
 		HotelRoom r2 = new HotelRoom(RoomTypes.DOUBLE, 234, 200); // Note the new RoomTypes enum. This will help prevent typos when creating Rooms.
@@ -33,6 +33,29 @@ public class main{
 		r1.Checkout();
 		*/
 		// End Test code
+		
+		
+		HotelRoom r1 = new HotelRoom(RoomTypes.SINGLE, 100, 100);
+		r1.AddRoom();
+		r1.Reserve(LocalDate.of(2024, 4, 17), LocalDate.of(2024, 4, 20));
+		
+		HotelRoom r2 = new HotelRoom(RoomTypes.SINGLE, 100, 102);
+		r2.AddRoom();
+		r2.Reserve(LocalDate.of(2024, 1, 1), LocalDate.of(2024, 2, 2));
+		
+		System.out.println("Rooms Added.");
+		
+		//               Customer(String FName, String LName, String Email, String PhoneNum, String HotelRoom, String CreditCard)
+		Customer c1 = new Customer("Jane", "Doe", "jane@yahoo.com", "555-123-4567", "100", "657482910");
+		Customer c2 = new Customer("Aidan", "Fox", "test@gmail.com", "555-555-5555", "102", "1234567890");
+		c1.UpdateExcel();
+		c2.UpdateExcel();
+		
+		System.out.println("Customers Added.");
+		
+		Customer x = Customer.findCustomer("Fox", 102);
+		System.out.println(x.getCustomerCreditCard());
+		System.out.println(x.toString());
 		
 		
 		Application.launch(MainPageController.class, args);
